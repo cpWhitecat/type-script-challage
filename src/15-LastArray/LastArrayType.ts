b03c17604e81b5ed73ffbf0799c3a1e7978c48c6
cpWhitecat <19803cp@gmail.com>
2022-07-19 20:12:12 +0800

    update: two challages

---
+++ src/15-LastArray/LastArrayType.ts
@@ -0,0 +1,13 @@
+type Last<T extends any[]> = T extends [ ...infer set,infer D] ? D : never
+
+
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from '../../utils'
+
+type cases = [
+  Expect<Equal<Last<[3, 2, 1]>, 1>>,
+  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
+]
+
+
