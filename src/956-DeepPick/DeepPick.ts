02d69d2ee3334cdee2aa58375f89ee45efa0f3c4
cpWhitecat <19803cp@gmail.com>
2022-08-22 20:50:51 +0800

    update : one challage

--- src/956-DeepPick/DeepPick.ts
+++ src/956-DeepPick/DeepPick.ts
@@ -1,5 +1,16 @@
-type DeepPick<T , K> 
+type result<T extends object, K> = K extends `${infer L}.${infer R}` ? {
+    [P in keyof T as (P extends L ? L : never)]:T[P] extends object ? result<T[P],R> : T[P]
+}
 
+: K extends keyof T ? Record<K,T[K]> : unknown
+
+// 逆变
+
+type DeepPick <T extends object, K> = (K extends K ?((k:result<T,K>)=>any) : never) extends ((k:infer P)=>any) ? P :never
+
+
+// how to include key
+// 为什么union类型不会分配了
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../../utils'
@@ -25,6 +36,8 @@ type Obj = {
   }
 }
 
+
+// 这个题目有问题啊 如果那个
 type cases = [
   Expect<Equal<DeepPick<Obj, ''>, unknown>>,
   Expect<Equal<DeepPick<Obj, 'a'>, { a: number }>>,
@@ -32,3 +45,7 @@ type cases = [
   Expect<Equal<DeepPick<Obj, 'a' | 'obj.e'>, { a: number } & { obj: { e: string } }>>,
   Expect<Equal<DeepPick<Obj, 'a' | 'obj.e' | 'obj.obj2.i'>, { a: number } & { obj: { e: string } } & { obj: { obj2: { i: boolean } } }>>,
 ]
+
+type Test1 =DeepPick<Obj, 'a' | 'obj.e' | 'obj.obj2.i'>
+
+type Test2  =DeepPick<Obj, 'a'> | DeepPick<Obj,''>
