9ef8d0ccc2a9af67d48f076a6aba72a8c5d4aa6e
cpWhitecat <19803cp@gmail.com>
2022-08-24 21:28:17 +0800

    update : one challage(solution it so fast

---
+++ src/2828-classPublic/classPublic.ts
@@ -0,0 +1,24 @@
+type ClassPublicKeys<T> = T extends infer F ? keyof F : T 
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '../../utils'
+
+class A {
+  public str: string
+  protected num: number
+  private bool: boolean
+  constructor() {
+    this.str = 'naive'
+    this.num = 19260917
+    this.bool = true
+  }
+
+  getNum() {
+    return Math.random()
+  }
+}
+
+type cases = [
+  Expect<Equal<ClassPublicKeys<A>, 'str' | 'getNum'>>,
+]
