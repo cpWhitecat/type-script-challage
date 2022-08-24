d4234af8471296e465cda7a75813cc4290fa82a6
cpWhitecat <19803cp@gmail.com>
2022-08-24 21:24:22 +0800

    update : one challage

--- src/2822-split/split.ts
+++ src/2822-split/split.ts
@@ -1,10 +1,21 @@
-type Split<S extends string, SEP extends string , Cache extends string = ''> = S extends `${infer L}${infer R}` ? (
-    L extends SEP ? [Cache,...Split<`${R}`,SEP>] : [...Split<`${R}`,SEP,`${Cache}${L}`>]
-) : [Cache]
+import type { Equal, Expect } from '../../utils'
 
+// S extends `${infer L}${infer R}` ? (
+//     L extends SEP ? [Cache,...Split<`${R}`,SEP>] : [...Split<`${R}`,SEP,`${Cache}${L}`>]
+// ) : [Cache]
+// type stringToTuple<S> = S extends `${infer }`
+
+// 取出末尾['']
+type DelLast<T,SEP extends string> = SEP extends '' ? (T extends [...infer Rest , infer Last] ? Last extends '' ? [...Rest] : T :T ) : T
+
+type result<S extends string, SEP extends string> = S extends `${infer L}${SEP}${infer R}` ? [L,...Split<`${R}`,SEP>] : [S]
+
+type Split<S extends string, SEP extends string> = Equal<S,string> extends true ? S[] : DelLast<result<S,SEP>,SEP>
+
+// 东拼西凑算是解决了 ， 而且也算做了个优化
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '../../utils'
+
 
 type cases = [
   Expect<Equal<Split<'Hi! How are you?', 'z'>, ['Hi! How are you?']>>,
@@ -21,3 +32,5 @@ type Test1 =Split<'Hi! How are you?', 'z'>
 type Test2 = Split<'Hi! How are you?', ''>
 
 type Test3 = Split<string, 'whatever'>
+
+type Test4 = Split<'', 'z'>
