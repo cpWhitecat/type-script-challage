2e9b53b87555701d814a089312117d0ec3323848
cpWhitecat <19803cp@gmail.com>
2022-09-03 22:36:42 +0800

    update : one challage

--- src/8640-NumberRange/NumberRange.ts
+++ src/8640-NumberRange/NumberRange.ts
@@ -14,7 +14,7 @@
 
 // 那就tuple to union 吧
 type NumberRangeTuple<L extends number, H extends number , T extends any[] = [H]> = H extends L ? [...T,H] : NumberRangeTuple<L,MinusOne<H>,[...T,MinusOne<H>]>
-type NumberRange<L extends number, H extends number> = TupleToUnion<NumberRangeTuple<L,H>>
+export type NumberRange<L extends number, H extends number> = TupleToUnion<NumberRangeTuple<L,H>>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../../utils'
--- src/9155-validDate/validDate.ts
+++ src/9155-validDate/validDate.ts
@@ -1,8 +1,45 @@
-type ValidDate<T extends string> = any
+type stringToTuple<T extends string , TupleCache extends string[] = []> = T extends `${infer F}${infer next}` ? stringToTuple<`${next}`,[...TupleCache,`${F}`]> : TupleCache
 
+type MyInclude<T , U> = T extends U ? true :false
+type monthsMap = {
+  '01':31,
+  '02':28,
+  '03':31,
+  '04':30,
+  '05':31,
+  '06':30,
+  '07':31,
+  '08':31,
+  '09':30,
+  '10':31,
+  '11':30,
+  '12':31,
+}
+
+type ToNumber<T extends string> = T extends `${infer All extends number}` ? All : T
+
+type handleZero<T> = T extends `${infer F extends number}${infer next extends number}` ? F extends 0 ? next : ToNumber<T> : T
+
+type handle<T extends string> = 
+T extends `${infer A}${infer B}${infer C}` ? 
+  `${A}${B}` extends keyof monthsMap ?
+  MyInclude<handleZero<`${C}`>,NumberRange<1,monthsMap[`${A}${B}`]>>
+  : false
+: true
+
+// 对于'02' 像这种带0的数 使用4.8 特性 会类型推导成number 而不是具体数值 那只能做特殊处理了
+type test1<T> = T extends `${infer A extends number}${infer B extends number}${infer C}` ? C : false
+
+type Test2 = test1<'0102'>
+// type Second<T extends string , meta extends>
+
+type ValidDate<T extends string> = stringToTuple<T>['length'] extends 4 ? handle<T> : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '../../utils'
+import { Push } from '../3057-push/push'
+import { GreaterThan } from '../4425-GreaterThan/GreaterThan'
+import { NumberRange } from '../8640-NumberRange/NumberRange'
 
 type cases = [
   Expect<Equal<ValidDate<'0102'>, true>>,
